92a9d4076dd3963d362c081199d5139aa2d8e965
Sam Holmes <samholmes1337@gmail.com>
2016-02-22 11:52:52 +0000

    Tidy up index.js and fix assignment expressions

--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,38 @@
-var FileRemote = require('./file-transport')
-  , GitRemote = require('./git-transport')
-  , SSHRemote = require('./ssh-transport')
-  , SSH_REL = /^\s*(.+?)@(.+?)\:(.+?)\s*$/i
-  , SSH_ABS = /^\s*(?:ssh\:\/\/)(.+)@(.+?)\/(.+?)\s*$/i
-  , GIT = /^\s*(?:git\:\/\/)(.+?)\/(.+?)\s*$/i
-  , FILE = /^\s*(?:file\:\/\/)?(.+?)\s*$/i;
+var FileRemote = require('./file-transport'),
+    GitRemote = require('./git-transport'),
+    SSHRemote = require('./ssh-transport'),
+    SSH_REL = /^\s*(.+?)@(.+?)\:(.+?)\s*$/i,
+    SSH_ABS = /^\s*(?:ssh\:\/\/)(.+)@(.+?)\/(.+?)\s*$/i,
+    GIT = /^\s*(?:git\:\/\/)(.+?)\/(.+?)\s*$/i,
+    FILE = /^\s*(?:file\:\/\/)?(.+?)\s*$/i;
 
 function connect(url, opts) {
-  var match, k
-    , remoteOpts = {};
+    var match, k, remoteOpts = {};
 
-  for (k in opts) {
-    remoteOpts[k] = opts[k];
-  }
+    for (k in opts) {
+        remoteOpts[k] = opts[k];
+    }
 
-  if (match = SSH_REL.exec(url)) {
-    remoteOpts.user = match[1];
-    remoteOpts.host = match[2];
-    remoteOpts.path = match[3];
-    return new SSHRemote(remoteOpts);
-  } else if (match = SSH_ABS.exec(url)) {
-    remoteOpts.user = match[1];
-    remoteOpts.host = match[2];
-    remoteOpts.path = '/' + match[3];
-    return new SSHRemote(remoteOpts);
-  } else if (match = GIT.exec(url)) {
-    remoteOpts.host = match[1];
-    remoteOpts.path = match[2];
-    return new GitRemote(remoteOpts);
-  } else if (match = FILE.exec(url)) {
-    remoteOpts.path = match[1];
-    return new FileRemote(remoteOpts);
-  } else {
-    throw new Error('Invalid repository URL');
-  }
+    if (match == SSH_REL.exec(url)) {
+        remoteOpts.user = match[1];
+        remoteOpts.host = match[2];
+        remoteOpts.path = match[3];
+        return new SSHRemote(remoteOpts);
+    } else if (match == SSH_ABS.exec(url)) {
+        remoteOpts.user = match[1];
+        remoteOpts.host = match[2];
+        remoteOpts.path = '/' + match[3];
+        return new SSHRemote(remoteOpts);
+    } else if (match == GIT.exec(url)) {
+        remoteOpts.host = match[1];
+        remoteOpts.path = match[2];
+        return new GitRemote(remoteOpts);
+    } else if (match == FILE.exec(url)) {
+        remoteOpts.path = match[1];
+        return new FileRemote(remoteOpts);
+    } else {
+        throw new Error('Invalid repository URL');
+    }
 
 }
 
